dba6610c22e20389407e9651025f3fba617a30a3
Shabab Karim <skarim222@gmail.com>
2020-08-26 19:44:14 -0500

    integrated prettier

--- config.js
+++ config.js
@@ -1,49 +1,49 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
+    url: 'https://lumen.netlify.com',
+    pathPrefix: '/',
+    title: 'Blog by Shabab Karim',
+    subtitle: 'Software engineer and sports enthusiast',
+    copyright: '© All rights reserved.',
+    disqusShortname: '',
+    postsPerPage: 4,
+    googleAnalyticsId: 'UA-73379983-2',
+    useKatex: false,
+    menu: [
+        {
+            label: 'Articles',
+            path: '/',
+        },
+        {
+            label: 'About me',
+            path: '/pages/about',
+        },
+        {
+            label: 'Contact me',
+            path: '/pages/contacts',
+        },
+    ],
+    author: {
+        name: 'Shabab Karim',
+        photo: '/photo.jpg',
+        bio: 'Software engineer and sports enthusiast',
+        contacts: {
+            email: 'skarim222@gmail.com',
+            facebook: '#',
+            telegram: '#',
+            twitter: '#',
+            github: 'github.com/ShababKarim',
+            rss: '',
+            vkontakte: '',
+            linkedin: 'shabab-karim-43262413b',
+            instagram: '#',
+            line: '',
+            gitlab: '',
+            weibo: '',
+            codepen: '',
+            youtube: '',
+            soundcloud: '',
+        },
     },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
-};
+}
